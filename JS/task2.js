d55d60df61199620c9435d09344fd44c6b6fa6cd
Maxim Shulgin <66272852+Millenium3000@users.noreply.github.com>
2021-01-17 21:26:01 +0200

    fixing mist

--- JS/task2.js
+++ JS/task2.js
@@ -1,24 +1,17 @@
 const ingredients = [
-  "Картошка",
-  "Грибы",
-  "Чеснок",
-  "Помидоры",
-  "Зелень",
-  "Приправы",
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
 ];
-// Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li, после чего вставит все li за одну операцию в список ul.ingredients.Для создания DOM - узлов используй document.createElement().
 
-const ulRef = document.querySelector("#ingredients");
-// console.log(ulRef);
-
-const newArr = ingredients.map((element) => {
-  // console.log(element);
-  const newList = document.createElement("li");
-  // console.log(newList);
-  newList.append(element);
-  // console.log(newList);
-  return newList;
+const liTags = ingredients.map(item => {
+  const li = document.createElement('li');
+  li.textContent = item;
+  return li;
 });
 
-ulRef.append(...newArr);
-console.log(ulRef);
+const ingredientsRef = document.querySelector('#ingredients');
+ingredientsRef.append(...liTags);
--- JS/task3.js
+++ JS/task3.js
@@ -1,39 +1,106 @@
-// Задание 3
-// Напиши скрипт для создания галлереи изображений по массиву данных.
-
-// В HTML есть список ul#gallery.
-
-// <ul id="gallery"></ul>
-// Используй массив объектов images для создания тегов img вложенных в li. Для создания разметки используй шаблонные строки и insertAdjacentHTML().
-
-// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
-// Добавь минимальное оформление галереи флексбоксами или гридами через css-классы.
 const images = [
   {
-    // это src
     url:
-      "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    alt: "White and Black Long Fur Cat",
+      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
   },
   {
     url:
-      "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
   },
   {
     url:
-      "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    alt: "Group of Horses Running",
+      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
   },
 ];
 
-const ulRef = document.querySelector("#gallery");
+const galleryRef = document.querySelector('#gallery');
+galleryRef.classList.add('gallery');
+const imageGallery = images.map(image => (
+  image = `<li class="gallery__item"><img src="${image.url}" alt="${image.alt}" width = 100%></li>`)
 
-const imagesRef = images.map((obj) => {
-  obj = `<li class= "gallery__item"><img src ="${obj.url}" alt ="${obj.alt}" width = 400px></li> `;
-  return obj;
-});
-// console.log(imagesRef);
 
-ulRef.insertAdjacentHTML("afterbegin", imagesRef.join(""));
-console.log(ulRef);
+galleryRef.insertAdjacentHTML('afterbegin', imageGallery.join(''));
+
+
+
+
+
+
+
+
+
+// // Задание 3
+// // Напиши скрипт для создания галлереи изображений по массиву данных.
+
+// // В HTML есть список ul#gallery.
+
+// // <ul id="gallery"></ul>
+// // Используй массив объектов images для создания тегов img вложенных в li. Для создания разметки используй шаблонные строки и insertAdjacentHTML().
+
+// // Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// // Добавь минимальное оформление галереи флексбоксами или гридами через css-классы.
+// const images = [
+//   {
+//     // это src
+//     url:
+//       "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+//     alt: "White and Black Long Fur Cat",
+//   },
+//   {
+//     url:
+//       "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+//     alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+//   },
+//   {
+//     url:
+//       "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+//     alt: "Group of Horses Running",
+//   },
+// ];
+
+// const ulRef = document.querySelector("#gallery");
+
+// const imagesRef = images.map((obj) => {
+//   obj = `<li class= "gallery__item"><img src ="${obj.url}" alt ="${obj.alt}" width = 400px></li> `;
+//   return obj;
+// });
+// // console.log(imagesRef);
+
+// ulRef.insertAdjacentHTML("afterbegin", imagesRef.join(""));
+// console.log(ulRef);
+
+
+
+// const images = [
+//   {
+//     url:
+//       'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+//     alt: 'White and Black Long Fur Cat',
+//   },
+//   {
+//     url:
+//       'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+//     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+//   },
+//   {
+//     url:
+//       'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+//     alt: 'Group of Horses Running',
+//   },
+// ];
+
+// const galleryRef = document.querySelector('#gallery');
+// galleryRef.classList.add('gallery');
+// const imageGallery = images.map(image => (
+//   image = `<li class="gallery__item"><img src="${image.url}" alt="${image.alt}" width = 100%></li>`)
+//   // Альтернативное решение
+//   /* const img = document.createElement('img')
+//   img.setAttribute('src', image.url);
+//   img.setAttribute('alt', image.alt);
+//   item.append(img); */
+// );
+
+// galleryRef.insertAdjacentHTML('afterbegin', imageGallery.join(''));
